03231ec27bdcbdfea5e68942b392b2aa7138c9af
Brandon May <bmay@yext.com>
2018-11-08 12:13:29 -0500

    Add PoC for running Python target from Bazel via subprocess

---
+++ foo_checker/bazel_run.py
@@ -0,0 +1,25 @@
+import subprocess
+import sys
+
+def execute(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    # Poll process for new output until finished
+    while True:
+        nextline = process.stdout.readline()
+        if nextline == '' and process.poll() is not None:
+            break
+        sys.stdout.write(nextline)
+        sys.stdout.flush()
+
+    output = process.communicate()[0]
+    exitCode = process.returncode
+
+    if (exitCode == 0):
+        return output
+    else:
+        raise ProcessException(command, exitCode, output)
+
+if __name__ == "__main__":
+    args = sys.argv
+    execute("bazel run {0}".format(args[1]))
